64f2e9bd12e274c81163c30b9b88ce8a4839a3c3
Abdu Alqouzi <aalqouzi@hotmail.com>
2020-12-04 15:55:58 +0300

    Add project files.

---
+++ ZoomViewApp/ZoomViewApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ZoomViewApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
